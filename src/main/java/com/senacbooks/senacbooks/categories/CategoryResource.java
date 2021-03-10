ace434bad6f80c50c32393ad6ca96c86f8476d3e
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-03-09 21:32:40 -0300

    adicionando metodo update

--- src/main/java/com/senacbooks/senacbooks/categories/CategoryResource.java
+++ src/main/java/com/senacbooks/senacbooks/categories/CategoryResource.java
@@ -1,17 +1,18 @@
 package com.senacbooks.senacbooks.categories;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -41,4 +42,10 @@ public class CategoryResource {
     }
 
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update (@PathVariable Long id, @RequestBody CategoryDTO body){
+        CategoryDTO dto = service.update(id, body);
+
+        return ResponseEntity.ok().body(dto);
+    }
 }
