c25e81144f9089c642c776cec1871c83fc31224a
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-03-23 00:07:46 -0300

    criando user-repository

---
+++ src/main/java/com/senacbooks/senacbooks/users/UserRepository.java
@@ -0,0 +1,9 @@
+package com.senacbooks.senacbooks.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
