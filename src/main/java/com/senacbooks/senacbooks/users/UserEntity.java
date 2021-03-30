d80f946d9844f95d34a2d016085bb1c97629fe08
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-03-29 22:31:07 -0300

    Adicionando campo de endereço

--- src/main/java/com/senacbooks/senacbooks/users/UserEntity.java
+++ src/main/java/com/senacbooks/senacbooks/users/UserEntity.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.senacbooks.senacbooks.roles.RoleEntity;
@@ -24,22 +25,22 @@ public class UserEntity implements Serializable {
     private String name;
     private String login;
     private String password;
-    private String email;
     private Boolean status;
 
     @ManyToOne
     @JoinColumn(name = "role_id")
     private RoleEntity role;
 
+    // private Address address;
+
     public UserEntity() {
     }
 
-    public UserEntity(Long id, String name, String login, String password, String email, Boolean status) {
+    public UserEntity(Long id, String name, String login, String password, Boolean status) {
         this.id = id;
         this.name = name;
         this.login = login;
         this.password = password;
-        this.email = email;
         this.status = status;
     }
 
@@ -73,14 +74,6 @@ public class UserEntity implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public Boolean getStatus() {
