6fec1c7fe2905ea9ba1b298eae813417365be41b
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-03-30 23:34:14 -0300

    Adicionando CRUD de endereço

---
+++ src/main/java/com/senacbooks/senacbooks/address/AddressDTO.java
@@ -0,0 +1,146 @@
+package com.senacbooks.senacbooks.address;
+
+import java.io.Serializable;
+
+import org.apache.catalina.User;
+
+public class AddressDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String zipCode;
+    private String address;
+    private Integer number;
+    private String addressComplement;
+    private String city;
+    private String state;
+    private String country;
+
+    private User user;
+
+    // private Client obj;
+
+    public AddressDTO() {
+    }
+
+    public AddressDTO(Long id, String zipCode, String address, Integer number, String addressComplement, String city,
+            String state, String country) {
+        this.id = id;
+        this.zipCode = zipCode;
+        this.address = address;
+        this.number = number;
+        this.addressComplement = addressComplement;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public AddressDTO(AddressEntity entity) {
+        this.id = entity.getId();
+        this.zipCode = entity.getZipCode();
+        this.address = entity.getAddress();
+        this.number = entity.getNumber();
+        this.addressComplement = entity.getAddressComplement();
+        this.city = entity.getCity();
+        this.state = entity.getState();
+        this.country = entity.getCountry();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getAddressComplement() {
+        return addressComplement;
+    }
+
+    public void setAddressComplement(String addressComplement) {
+        this.addressComplement = addressComplement;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AddressDTO other = (AddressDTO) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ src/main/java/com/senacbooks/senacbooks/address/AddressEntity.java
@@ -0,0 +1,150 @@
+package com.senacbooks.senacbooks.address;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.apache.catalina.User;
+
+@Entity
+@Table(name = "tb_address")
+public class AddressEntity implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String zipCode;
+    private String address;
+    private Integer number;
+    private String addressComplement;
+    private String city;
+    private String state;
+    private String country;
+
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // @ManyToOne
+    // @JoinColumn(name = "client_id")
+    // private Client client;
+
+    public AddressEntity() {
+    }
+
+    public AddressEntity(Long id, String zipCode, String address, Integer number, String addressComplement, String city,
+            String state, String country) {
+        this.id = id;
+        this.zipCode = zipCode;
+        this.address = address;
+        this.number = number;
+        this.addressComplement = addressComplement;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getAddressComplement() {
+        return addressComplement;
+    }
+
+    public void setAddressComplement(String addressComplement) {
+        this.addressComplement = addressComplement;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AddressEntity other = (AddressEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
