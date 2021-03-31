d7b82ec3952502d77cc4acf96bab7d345d218225
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-03-31 00:19:31 -0300

    Adicionando campo cpf e método findAllPaged

--- src/main/java/com/senacbooks/senacbooks/users/UserService.java
+++ src/main/java/com/senacbooks/senacbooks/users/UserService.java
@@ -3,6 +3,8 @@ package com.senacbooks.senacbooks.users;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +14,16 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Transactional(readOnly = true) 
+    public Page<UserDTO> findAllPaged(PageRequest pageRequest) {
+        Page<UserEntity> list = repository.findAll(pageRequest);
+        return list.map(x -> new UserDTO(x));
+    }
+
     @Transactional(readOnly = true)
     public UserDTO findById(Long id) {
         Optional<UserEntity> obj = repository.findById(id);
         UserEntity entity = obj.orElseThrow();
-
         return new UserDTO(entity);
     }
 
@@ -25,7 +32,6 @@ public class UserService {
         UserEntity entity = new UserEntity();
         copyDTOToEntity(dto, entity);
         entity = repository.save(entity);
-
         return new UserDTO(entity);
     }
 
@@ -34,7 +40,6 @@ public class UserService {
         UserEntity entity = repository.getOne(id);
         copyDTOToEntity(dto, entity);
         entity = repository.save(entity);
-
         return new UserDTO(entity);
     }
 
@@ -48,14 +53,16 @@ public class UserService {
             entity = repository.save(entity);
             message = "Usuário " + entity.getName() + " inativado com sucesso";
         } else {
-            message = "O usuário " + entity.getName() + " já está inativo";
+            entity.setStatus(true);
+            entity = repository.save(entity);
+            message = "Usuário " + entity.getName() + " Ativado com sucesso";
         }
-
         return message;
     }
 
     private void copyDTOToEntity(UserDTO dto, UserEntity entity) {
         entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
         entity.setLogin(dto.getLogin());
         entity.setPassword(dto.getPassword());
         entity.setStatus(dto.getStatus());
