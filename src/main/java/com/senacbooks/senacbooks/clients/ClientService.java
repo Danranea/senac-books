241ac475cda54da5ae8a7ba1972e255cf5c7cce7
João Roschel <joaomroschel@gmail.com>
2021-04-14 19:10:40 -0300

    agora vai! (?)

--- src/main/java/com/senacbooks/senacbooks/clients/ClientService.java
+++ src/main/java/com/senacbooks/senacbooks/clients/ClientService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class ClientService {
     
     @Autowired
