c07b6c8c05ec3332f4efbd05f4fbda8f084d8f65
Sillas Cavalcante <sillas.ap16@gmail.com>
2021-05-24 23:24:00 -0300

    Ao efetuar uma compra, atualiza a quantidade disponivel dos produtos comprados

--- src/main/java/com/senacbooks/senacbooks/orders/details/OrderDetailsService.java
+++ src/main/java/com/senacbooks/senacbooks/orders/details/OrderDetailsService.java
@@ -2,8 +2,10 @@ package com.senacbooks.senacbooks.orders.details;
 
 import com.senacbooks.senacbooks.orders.OrdersEntity;
 import com.senacbooks.senacbooks.orders.OrdersRepository;
+import com.senacbooks.senacbooks.products.ProductDTO;
 import com.senacbooks.senacbooks.products.ProductEntity;
 import com.senacbooks.senacbooks.products.ProductRepository;
+import com.senacbooks.senacbooks.products.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,6 +25,9 @@ public class OrderDetailsService {
   @Autowired
   private ProductRepository productRepository;
 
+  @Autowired
+  private ProductService productService;
+
   @Transactional
   public Page<OrderDetailsDTOOut> findAllPaged(PageRequest pageRequest) {
     Page<OrderDetailsEntity> list = orderDetailsRepository.find(pageRequest);
@@ -34,19 +39,30 @@ public class OrderDetailsService {
   public OrderDetailsDTO insert(OrderDetailsDTO dto) {
     OrderDetailsEntity entity = new OrderDetailsEntity();
     copyDTOToEntity(dto, entity);
+    updateProductQuantity(dto.getProduct(), dto.getQuantity());
     entity = orderDetailsRepository.save(entity);
-
-    return new OrderDetailsDTO(entity);
+    
+    return new OrderDetailsDTO(entity); 
   }
 
   private void copyDTOToEntity(OrderDetailsDTO dto, OrderDetailsEntity entity) {
     OrdersEntity ordersEntity = ordersRepository.getOne(dto.getOrder().getId());
     entity.setOrder(ordersEntity);
-
+    
     ProductEntity productEntity = productRepository.getOne(dto.getProduct().getId());
     entity.setProduct(productEntity);
 
     entity.setQuantity(dto.getQuantity());
+  }
+
+  private void updateProductQuantity(ProductDTO product, Integer quantity) {
+    if (product.getQuantity() >= quantity) {
+      Integer result = product.getQuantity() - quantity;
+      product.setQuantity(result);
+    } else {
+      return;
+    }
+    productService.update(product.getId(), product);
   }
 
 }
